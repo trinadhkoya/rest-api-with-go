74263ae2f071b5302c1d22f69911ddb1c4bf69a4
Trinadh Koya <trinadhkoya@Trinadhs-MacBook-Pro.local>
2021-04-11 11:33:14 +0530

    port chnages

--- main.go
+++ main.go
@@ -7,12 +7,11 @@ import (
 
 
 func main() {
+
 	http.HandleFunc("/", helloWorld)
-	http.HandleFunc("/status", helloWorld)
-	http.ListenAndServe(":5000", nil)
+	http.ListenAndServe(":5001", nil)
 }
 
 func helloWorld(writer http.ResponseWriter, request *http.Request) {
-	fmt.Println(request)
-	_, _ = fmt.Fprintf(writer, "<h1>Hello! It's up and running</h1>")
+	fmt.Fprintf(writer, "Hello! Go...")
 }
