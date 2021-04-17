a4a436de28d1f4c6f51debc30c063d4abf5dae47
Trinadh Koya <trinadhkoya@Trinadhs-MacBook-Pro.local>
2021-04-17 12:13:00 +0530

    minor changes

--- main.go
+++ main.go
@@ -1,20 +1,27 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"github.com/gin-gonic/gin"
-	"log"
+	"rest-api/config"
 	"rest-api/controller"
 )
 
 func main() {
+	environment := flag.String("e", "development", "")
+	config.Init(*environment)
+
 	r := gin.Default()
+
 	r.GET("/", controller.Welcome)
 	r.GET("/ping", controller.Ping)
 	r.GET("/health", controller.Status)
 
 	r.NoRoute(controller.Show404)
-	if err := r.Run(); err != nil {
-		log.Fatal("Looks like something went wrong ")
+
+	err:= r.Run(config.GetConfig().GetString("server_port"));if err!=nil{
+		fmt.Println("Looks like something went wrong ")
 	}
 
 }
